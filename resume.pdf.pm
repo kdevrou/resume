2c7c21745263f720950e574636c89f09841fd955
Kevin DeVrou <kevin.devrou@gmail.com>
2021-02-17 23:24:13 -0500

    add basic layout

--- resume.pdf.pm
+++ resume.pdf.pm
@@ -17,53 +17,74 @@ github.com/kdevrou
 
 ◊h2{Experience}
 
-Senior Software Engineer
+◊strong{Senior Software Engineer}
+
 Kunz Leigh & Associates
+
 Jun 2017 - Present
 
-Senior Software Engineer
+◊strong{Senior Software Engineer}
+
 Teksystems
+
 Feb 2017 - Mar 2017 (2 months)
 
-Software Engineer III
+◊strong{Software Engineer III}
+
 Dematic
+
 Mar 2016 - Feb 2017 (1 year)
 
-Java Developer
+◊strong{Java Developer}
+
 RecogNation
+
 Oct 2013 - Mar 2016 (2 years 6 months)
+
 In my position at Hi5 I have had the chance to help improve and expand on an existing employee
 recognition platform. My major accomplishments so far have been:
-- Created version control standards and deployment procedures for existing projects
-- Reduced non-cached page load times by 82%
-- Shortened project cycle times
-- Created processes to automate daily tasks such as ordering and pulling inventory updates using Scala
-+ Play Framework
 
-Software Developer I
+◊ul{
+	◊li{Created version control standards and deployment procedures for existing projects}
+	◊li{Reduced non-cached page load times by 82%}
+	◊li{Shortened project cycle times}
+	◊li{Created processes to automate daily tasks such as ordering and pulling inventory updates using Scala
++ Play Framework}
+
+}
+
+◊strong{Software Developer I}
+
 Jackson
+
 Jun 2011 - Oct 2013 (2 years 5 months)
+
 Working mostly with Java to create document workflow systems. During my time at Jackson I had the
 opportunity to:
-- Coordinated releases for team of 50+ developers
-- Build a system dashboard using node.js + socketio
-- Convert several major applications from Websphere to Jboss
+◊ul{
+	◊li{Coordinated releases for team of 50+ developers}
+	◊li{Build a system dashboard using node.js + socketio}
+	◊li{Convert several major applications from Websphere to Jboss}
+}
 
 ◊h2{Education}
 
-Davenport University
+◊strong{Davenport University}
+
 Bachelor of Applied Science (B.A.Sc.), Computer Information System
+
 2007 - 2011
 
 ◊h2{Skills}
-
-SQL
-Java
-Scala
-Linux
-JUnit
-Web Services
-Docker
-JavaScript
-AJAX
-Play Framework
+◊ul{
+	◊li{SQL}
+	◊li{Java}
+	◊li{Scala}
+	◊li{Linux}
+	◊li{JUnit}
+	◊li{Web Services}
+	◊li{Docker}
+	◊li{JavaScript}
+	◊li{AJAX}
+	◊li{Play Framework}
+}
